ec260c839c00c6c2d8d23b8863572a2a8bae4b07
BerkanAcar <berkanacar93@gmail.com>
2016-07-15 10:40:16 +0300

    tweet model'i oluşturuldu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714104930) do
+ActiveRecord::Schema.define(version: 20160714110926) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
